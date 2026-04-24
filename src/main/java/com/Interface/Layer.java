b41fb9e85db1719e3a80236e20a27b9a4039c1fe
LeottaAlberto <leottaalberto43@gmail.com>
2026-04-24 18:17:54 +0200

    feat(Layer.java): change logic

--- src/main/java/com/Interface/Layer.java
+++ src/main/java/com/Interface/Layer.java
@@ -1,7 +1,11 @@
 package com.Interface;
 
+/**
+ * @param I = Input data from upper layer
+ * @param O = Output data from down layer
+ */
 public interface Layer {
-    public void send();
+    public void send(Packet packet);
 
-    public void receive();
+    public void receive(Packet packet);
 }
