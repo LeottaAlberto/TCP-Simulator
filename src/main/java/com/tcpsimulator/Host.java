14121342a65107217a16b1f60e688573cc73cdcf
LeottaAlberto <leottaalberto43@gmail.com>
2026-04-24 10:12:16 +0200

    feat(Host.java): add getter, setter and constructor. Write Javadoc

--- src/main/java/com/tcpsimulator/Host.java
+++ src/main/java/com/tcpsimulator/Host.java
@@ -1,14 +1,86 @@
 package com.tcpsimulator;
 
-import com.layer.*;
+import java.util.UUID;
+
+import com.layer.LayerInfo;
 
 public class Host {
+    public static final int MAX_HOST_NAME_LENGTH = 20;
+
     private String name;
     private boolean status;
-    private LayerInfo info;
+    private LayerInfo network;
 
-    public Host(String name, String ip, String mac){
-        this.name = name;
-        this.info = new LayerInfo(this, ip, mac);
+    /**
+     * Normal constructor
+     *
+     * @param name hostname must not be empty or longer than MAX_HOST_NAME_LENGTH
+     * @param ip   is a string like this 192.168.100.234
+     * @param mac  is a string like this E4-60-17-3A-6F-6E
+     */
+    public Host(String name, String ip, String mac) {
+        this.name = (name.isEmpty() || name.length() > 20)
+                ? "host-" + UUID.randomUUID()
+                : name;
+        this.network = new LayerInfo(this, ip, mac);
     }
+
+    /**
+     * Copy constructor
+     * 
+     * @param ref another Host
+     */
+    public Host(Host ref) {
+        this.name = ref.getName();
+        this.network = new LayerInfo(ref, ref.getNetwork().getIP(), ref.getNetwork().getMAC());
+    }
+
+    // Getter
+
+    public String getName() {
+        return this.name;
+    }
+
+    public boolean isStatus() {
+        return this.status;
+    }
+
+    public LayerInfo getNetwork() {
+        return this.network;
+    }
+
+    // Setter
+
+    /**
+     * change hostname. The hostname must not be empty or longer than
+     * MAX_HOST_NAME_LENGTH
+     * 
+     * @param name hostname
+     */
+    public void setName(String name) {
+        if (name.isEmpty() || name.length() > 20)
+            return;
+
+        this.name = name;
+    }
+
+    /**
+     * Turn off or turn on the host
+     */
+    public void switchStatus() {
+        this.status = !this.status;
+    }
+
+    /**
+     * Swap the network card for another, but only the host is off
+     * 
+     * @param network network card
+     */
+    public void setNetwork(LayerInfo network) {
+        if (this.status)
+            return;
+
+        this.network = network;
+    }
+
 }
