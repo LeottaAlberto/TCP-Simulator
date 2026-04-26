d8a3270db4afa455dad21c9e83379182f5a8dc12
LeottaAlberto <leottaalberto43@gmail.com>
2026-04-27 00:01:31 +0200

    feat(Application.java): implement Packet interface and add message handling methods

--- src/main/java/com/package_layer/Application.java
+++ src/main/java/com/package_layer/Application.java
@@ -1,4 +1,28 @@
 package com.package_layer;
 
-public class Application {
+import com.Enums.ApplicationProtocol;
+import com.Interface.Packet;
+
+public class Application implements Packet<Application> {
+    private final String mess;
+    private final ApplicationProtocol prot;
+
+    public Application(String mess, ApplicationProtocol prot) {
+        this.mess = mess;
+        this.prot = prot;
+    }
+
+    public String getMess() {
+        return this.mess;
+    }
+
+    public ApplicationProtocol getProt() {
+        return this.prot;
+    }
+
+    @Override
+    public Application getPayload() {
+        return this;
+    }
+
 }
