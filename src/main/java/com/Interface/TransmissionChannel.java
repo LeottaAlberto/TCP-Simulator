d6cdb09aa5b0e5eb2ff000361e928a1dbad9ae98
LeottaAlberto <leottaalberto43@gmail.com>
2026-04-27 23:51:43 +0200

    feat(TransmissionChannel.java): change return typo

--- src/main/java/com/Interface/TransmissionChannel.java
+++ src/main/java/com/Interface/TransmissionChannel.java
@@ -3,7 +3,7 @@ package com.Interface;
 import com.package_layer.Frame;
 
 public interface TransmissionChannel {
-    void sendOnWire(Frame frame);
+    boolean sendOnWire(Frame frame);
 
-    void sendBroadcast(Frame frame);
+    boolean sendBroadcast(Frame frame);
 }
